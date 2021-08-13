0639795f61df58d470be2eab317341d3ad3fa566
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-08-13 17:53:00 +0530

    Updates

---
+++ precache-manifest.62056015d5b2b6df255670eea49a5a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84c7d00cee1ab6c50ca31782d545aa6",
+    "url": "/CodeCamp_1-Birthday_Reminder/index.html"
+  },
+  {
+    "revision": "65587f2bed5479861ab1",
+    "url": "/CodeCamp_1-Birthday_Reminder/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/CodeCamp_1-Birthday_Reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodeCamp_1-Birthday_Reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65587f2bed5479861ab1",
+    "url": "/CodeCamp_1-Birthday_Reminder/static/js/main.38d5966b.chunk.js"
+  },
+  {
+    "revision": "6c6b873e6a9fea7087da",
+    "url": "/CodeCamp_1-Birthday_Reminder/static/js/runtime-main.0747191f.js"
+  }
+]);
